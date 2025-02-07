c665f26ac71caeb9fcd94512e619d295f64807e5
Jessamy Guzman <jessamyguzman@blue.gen.in>
2025-02-07 00:35:31 -0600

    Initial Commit

---
+++ GameTower/GameTowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTowerApp.swift
+//  GameTower
+//
+//  Created by Ruben Manuel Elizalde Salazar on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GameTowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
